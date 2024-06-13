4ef7bb392d44b0a236ad52784343cc17a1e20c63
VikiPotapova <viki.patapava@gmail.com>
2024-06-13 20:02:33 +0200

    Initial commit

---
+++ src/main/java/com/example/weatherconsumer/WeatherConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.weatherconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherConsumerApplication.class, args);
+
+    }
+}
